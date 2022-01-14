683aa1a8273454c1b674d2f54effe117a7d1a76b
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-01-14 16:42:56 -0500

    findKey.js file

---
+++ findKey.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+// //     findKey function      // //
+// the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. 
+// If no key is found, then it should return undefined
+
+const findKey = function (obj, callback) {   
+  for (let key in obj) {      
+    if(callback(obj[key])) {      
+      return key;
+    }    
+  }
+  return undefined;
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
